2fbb8d8ef58f4e2ea1417259d3073742d9b7faa1
gankochann <phamngan0208hy@gmail.com>
2025-05-24 11:42:03 +0900

    Add files via upload

---
+++ TodoList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ContainList from './ContainList'
+import ListItem from './ListItem'
+
+export default function TodoList() {
+  return (
+    <div>
+        <>
+  <ContainList/>
+  <ListItem/>
+</>
+
+    </div>
+  )
+}
